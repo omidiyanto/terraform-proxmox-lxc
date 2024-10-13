5c4e8c299eb92fa0ff53149dca26cbbc4010a52a
O. Midiyanto <omidiyanto7@gmail.com>
2024-10-13 19:35:33 +0700

    first commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc4"
+    }
+  }
+}
+
+resource "proxmox_lxc" "basic" {
+  target_node  = "proxmox"
+  hostname     = "lxc-basic"
+  ostemplate   = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+  password     = "ubuntu"
+  unprivileged = true
+
+  // Terraform will crash without rootfs defined
+  rootfs {
+    storage = "local"
+    size    = "8G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+}

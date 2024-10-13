39d86155f1791d7de95332518ac8e7d9f2a3e1ba
O. Midiyanto <omidiyanto7@gmail.com>
2024-10-13 19:37:02 +0700

    fix

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "proxmox" {
     pm_tls_insecure = true
-    pm_api_url = "https://proxmox.omidiyanto.my.id/api2/json"
+    pm_api_url = "https://PROXMOX_URL_SERVER:8006/api2/json"
     pm_api_token_id = "root@pam!terraform"
-    pm_api_token_secret = "7ecfd4e4-87b9-44f3-96e2-b0e1dda5230a"
+    pm_api_token_secret = "xxxx"
 }
